9877ba415b2db97f3885897442f3ae35b1c5873a
Adrian L Lange <adrianlund@gmail.com>
2014-04-10 05:10:31 +0200

    Display the number of feeds in the pageAction title

--- scripts/background.js
+++ scripts/background.js
@@ -2,6 +2,10 @@ var feeds = {};
 chrome.runtime.onMessage.addListener(function(message, sender){
 	feeds[sender.tab.id] = message;
 	chrome.pageAction.show(sender.tab.id);
+	chrome.pageAction.setTitle({
+		tabId: sender.tab.id,
+		title: 'Found ' + message.length + ' feed' + (message.length > 1 ? 's' : '') + '!'
+	});
 });
 
 chrome.tabs.onRemoved.addListener(function(tabId){
