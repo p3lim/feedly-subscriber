e3dcf8606e5e5c26148c113b60dc3a933065062d
Adrian L Lange <mail@p3lim.net>
2015-01-30 13:37:22 +0100

    Improve the sniffer

--- scripts/sniffer.js
+++ scripts/sniffer.js
@@ -1,12 +1,34 @@
-// does not handle full-document feeds
-var xpath = '//link[contains(@rel, "alternate")][contains(@type, "rss") or contains(@type, "atom") or contains(@type, "rdf")]';
-var result = document.evaluate(xpath, document, null, 0, null);
-
-var item, count = 0, feeds = [];
-while(item = result.iterateNext()){
-	feeds.push({'href': item.href, 'title': item.title});
-	count++;
+var hasFeed = function(dom){
+	var result = dom.evaluate('//*[local-name()="rss" or local-name()="feed" or local-name()="RDF"]', dom, null, 0, null);
+	if(result){
+		var node = result.iterateNext();
+		if(node && !(node.parentElement && node.parentElement.tagName != 'BODY'))
+			return true;
+	};
 };
 
-if(count > 0)
-	chrome.runtime.sendMessage(feeds);
+if(document.body.childElementCount == 1){
+	var tagName = document.body.children[0].tagName;
+
+	if(tagName == 'RSS' || tagName == 'FEED' || tagName == 'RDF')
+		location.href = 'https://feedly.com/i/subscription/feed/' + escape(location.href);
+	else if(tagName == 'PRE'){
+		var parser = new DOMParser();
+		var dom = parser.parseFromString(document.body.textContent, 'text/xml');
+		if(hasFeed(dom))
+			location.href = 'https://feedly.com/i/subscription/feed/' + escape(location.href);
+	};
+} else {
+	var feeds = [];
+	[].forEach.call(document.querySelectorAll('link'), function(link){
+		if(link.rel == 'alternate' && (link.type.indexOf('rss') != -1 || link.type.indexOf('atom') != -1)){
+			feeds.push({
+				href: link.href,
+				title: link.title
+			});
+		};
+	});
+
+	if(feeds.length)
+		chrome.runtime.sendMessage(feeds);
+}
